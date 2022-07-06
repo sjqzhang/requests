5fd6834b01828a6916a4a786e8aedfae31f8c461
junqiang.zhang <junqiang.zhang@shopee.com>
2022-07-06 12:15:55 +0800

    Revert "add NewRecorder for test"
    This reverts commit 257d3c0c6cfed43ff9f9372abeafe05e61172386.

--- requests.go
+++ requests.go
@@ -25,7 +25,6 @@ import (
 	"mime/multipart"
 	"net/http"
 	"net/http/cookiejar"
-	"net/http/httptest"
 	"net/http/httputil"
 	"net/url"
 	"os"
@@ -85,7 +84,7 @@ func Requests() *Request {
 
 func NewRequestForTest(method, origurl string, args ...interface{}) *http.Request {
 	req := Requests()
-	req.httpreq.Method = strings.ToUpper(method)
+	req.httpreq.Method = "POST"
 	//set default
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	// set params ?a=b&b=c
@@ -118,10 +117,6 @@ func NewRequestForTest(method, origurl string, args ...interface{}) *http.Reques
 		case Auth:
 			// a{username,password}
 			req.httpreq.SetBasicAuth(a[0], a[1])
-		case io.ReadCloser:
-			req.httpreq.Body = a
-		default:
-			panic("args not support!")
 		}
 	}
 
@@ -140,10 +135,6 @@ func NewRequestForTest(method, origurl string, args ...interface{}) *http.Reques
 	}
 	req.httpreq.URL = URL
 	return req.httpreq
-}
-
-func NewRecorder() *httptest.ResponseRecorder {
-	return httptest.NewRecorder()
 }
 
 // Get ,req.Get
